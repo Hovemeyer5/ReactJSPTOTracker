d9d46deafd6349e8a6484e97067968ec63c4b323
Unknown <hovemeyer5@marshall.edu>
2018-10-31 15:06:51 -0400

    Retrieved articles

--- pto-tracker/src/index.js
+++ pto-tracker/src/index.js
@@ -4,16 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { promiseMiddleware } from './middleware';
 
 const defaultState = { 
     appName: 'PTO Tracker',
     timeEntries: null,
 };
 const reducer = function (state = defaultState, action) {
+    switch(action.type) {
+        case 'HOME_PAGE_LOADED':
+         return { ...state, articles: action.payload.articles };
+    }
     return state;
 };
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));;
 
 ReactDOM.render((
     <Provider store={store}>
