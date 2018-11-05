bdfe3dd23e90ea1df5b889a34d0edfd7210ddee2
Unknown <hovemeyer5@marshall.edu>
2018-11-05 07:48:01 -0500

    moving store to store file

--- pto-tracker/src/index.js
+++ pto-tracker/src/index.js
@@ -4,28 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { promiseMiddleware } from './middleware';
+import store from './store';
 
-const defaultState = { 
-    appName: 'PTO Tracker',
-    timeEntries: null,
-};
-const reducer = function (state = defaultState, action) {
-    switch(action.type) {
-        case 'HOME_PAGE_LOADED':
-         return { ...state, articles: action.payload.articles };
-    }
-    return state;
-};
-const store = createStore(reducer, applyMiddleware(promiseMiddleware));;
 
 ReactDOM.render((
     <Provider store={store}>
         <App />
 </Provider>), document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ pto-tracker/src/store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from 'redux';
+import { promiseMiddleware } from './middleware';
+
+const defaultState = { 
+    appName: 'PTO Tracker',
+    timeEntries: null,
+};
+const reducer = function (state = defaultState, action) {
+    switch(action.type) {
+        case 'HOME_PAGE_LOADED':
+         return { ...state, articles: action.payload.articles };
+    }
+    return state;
+};
+
+const middleware = applyMiddleware(promiseMiddleware);
+
+const store = createStore(reducer, middleware);
+
+
+export default store;
