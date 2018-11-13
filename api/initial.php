cbfc012e6ea7142e0df5c83ce23da2036cf687bd
Unknown <hovemeyer5@marshall.edu>
2018-11-13 10:57:19 -0500

    Update initial php to make accrual

--- api/initial.php
+++ api/initial.php
@@ -16,10 +16,24 @@ class ptoEntry {
         $this->credit = $credit;
     }
 }
+
+$rollover = 40;
+$accrual_rate = 13.33;
+
 $initial = array(
-    new ptoEntry(1, "01/01/19", "01/01/19", "Initial Balance", 0.00, 100),
-    new ptoEntry(2, "02/01/19", "02/01/19", "Credit for February", 0.00, 15),
+    new ptoEntry(1, "01/01/19", "01/01/19", "Initial Balance", 0.00, $rollover)
 );
 
+for($i = 1; $i <= 12; $i++){
+    $month = $i < 10 ? "0" . $i : $i;
+    $startDate = date('m/d/Y', mktime(0, 0, 0, $month, 1, 2019));
+    $endDate = $startDate;
+    array_push(
+        $initial,
+        new ptoEntry($i + 1, $startDate, $endDate, $startDate . " Acrrual", 0.00, $accrual_rate)
+    );
+}
+
 echo json_encode($initial);
+
 ?>
