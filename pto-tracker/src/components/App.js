bcde27609063e37a8e9ac90d59064ba6d12cac5b
Unknown <hovemeyer5@marshall.edu>
2018-11-19 15:21:21 -0500

    rename for consistency

--- pto-tracker/src/components/App.js
+++ pto-tracker/src/components/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Header from './Header';
-import Login from './user_auth/Login';
-import Registration from './user_auth/Registration';
+import Login from './auth/Login';
+import Registration from './auth/Registration';
 import EmployeeView from './EmployeeView';
 
 
