b5b989ffb36ec0a761c9a2845f1825b043d97210
Unknown <hovemeyer5@marshall.edu>
2018-11-28 14:13:23 -0500

    Refactor code onto Employee Class

--- pto-tracker/src/components/PtoGeneralInfo.js
+++ pto-tracker/src/components/PtoGeneralInfo.js
@@ -11,40 +11,8 @@ const mapStateToProps = state => {
 
 class PtoGeneralInfo extends Component {
    
-    componentDidUpdate(){
-        console.log(this.props.employee);
-    }
-    getEarnedBalance(entries){
-        if(!entries || entries.length === 0){
-            return 0;
-        }
-        let earnedBalance = 0;
-        let today = new Date().getTime();
-        let lastEntryBeforeToday = 0;
-        lastEntryBeforeToday = entries
-        .findIndex(entry => today > new Date(entry.endDate).getTime());
-        if(lastEntryBeforeToday > 0){
-            let pastEntries = entries.slice(0, lastEntryBeforeToday);
-            earnedBalance = pastEntries.map(e => e.credit).reduce((t,c) => t+c);
-        }
-        return Math.round(earnedBalance*100)/100;
-    }
-    getProjectedBalance(employee){
-        const projectedAccrual = employee.accrualRate * 12;
-        let usedPTO = 0;
-        if(employee.entries.length > 0){
-            usedPTO = employee.entries.map(entry => entry.debit).reduce((total, entry) => {
-                total = total || 0;
-                return total + entry;
-            });
-        }
-        const projectBalance = projectedAccrual - usedPTO;
-        return Math.round(projectBalance*100)/100;
-    }
     render() {
-            const employee = this.props.employee;
-            const earnedBalance = this.getEarnedBalance(employee.entries);
-            const projectedBalance = this.getProjectedBalance(employee);
+        const employee = this.props.employee;
         
         return (
             <div>
@@ -69,11 +37,11 @@ class PtoGeneralInfo extends Component {
                         </div>
                         <div className="col-md-4 middle">
                             <label>Earned PTO: </label>
-                            {earnedBalance}
+                            {employee.getEarnedBalance()}
                         </div>
                         <div className="col-md-4 last">
                             <label>Projected PTO: </label>
-                            {projectedBalance}
+                            {employee.getProjectedBalance()}
                         </div>
                     </div>
             </div>
--- pto-tracker/src/models/Employee.js
+++ pto-tracker/src/models/Employee.js
@@ -6,6 +6,36 @@ let Employee = function (employee = {}) {
     this.accrualRate = employee.accrualRate || 0;
     this.entries = employee.entries || [];
     this.requests = employee.requests || [];
+
+    this.getEarnedBalance = function(){
+        if(this.entries.length === 0){
+            return 0;
+        }
+        let earnedBalance = 0;
+        let today = new Date().getTime();
+        let lastEntryBeforeToday = 0;
+        lastEntryBeforeToday = this.entries
+        .findIndex(entry => today > new Date(entry.endDate).getTime());
+        if(lastEntryBeforeToday > 0){
+            let pastEntries = this.entries.slice(0, lastEntryBeforeToday);
+            earnedBalance = pastEntries.map(e => e.credit).reduce((t,c) => t+c);
+        }
+        return Math.round(earnedBalance*100)/100;
+    }
+    this.getProjectedBalance = function(){
+        if(this.entries.length === 0){
+            return 0;
+        }
+        const projectedAccrual = this.entries
+            .map(entry => entry.credit)
+            .reduce((t, c) => t+c);
+
+        const usedPTO = this.entries
+            .map(entry => entry.debit)
+            .reduce((t, d) => t+d);
+        const projectBalance = projectedAccrual - usedPTO;
+        return Math.round(projectBalance*100)/100;
+    }
 }
 
 export default Employee;
